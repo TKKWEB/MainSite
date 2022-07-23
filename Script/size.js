2f961d54975f1dc55d003d8e50d09bce503dc6af
Thommandru Kiran Kumar <109029074+TKKWEB@users.noreply.github.com>
2022-07-23 21:23:15 +0530

    Add files via upload

---
+++ Script/size.js
@@ -0,0 +1,8 @@
+window.onresize = screen;
+window.onload = screen;
+
+function screen(){
+    myWidth = window.innerWidth;
+    myHeight = window.innerHeight;
+    document.getElementById("size").innerHTML = "Width " + myWidth + "px, Height " + myHeight + "px." ;
+}
